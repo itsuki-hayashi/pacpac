d2024e396f1ea0dd6380c5bf124035df45ad2521
itsuki-hayashi <2121340+itsuki-hayashi@users.noreply.github.com>
2025-09-06 14:20:29 +0900

    Improve make_love_file.sh to fix shellcheck issues

--- make_love_file.sh
+++ make_love_file.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-#
-# Hehe.
-#
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-cd pacpac
-zip -r ../pacpac.love *
+#!/bin/bash\
+cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
+cd pacpac || exit
+zip -r ../pacpac.love ./*
